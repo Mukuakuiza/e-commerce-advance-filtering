4694b286cc963582ce194c634014e867442da11e
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-05-04 02:01:39 +0200

    created folders and components, and worked on navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Nav from "./Navegation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+
 function App() {
   return (
     <>
-      <h1>Hello </h1>
+      <Nav />
+      <Products />
+      <Recommended />
     </>
   );
 }
