bd96ad695e40241bf0ea376857082d36d60858bd
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-05-12 23:23:21 +0200

    worked on sidebar, price, and color components

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,7 +1,33 @@
 import "./Price.css";
 
 const Price = () => {
-  return <div>Price</div>;
+  return (
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
+
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test2" />
+        <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test2" />
+        <span className="checkmark"></span>R0 - R50
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test2" />
+        <span className="checkmark"></span>R50 - R100
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test2" />
+        <span className="checkmark"></span>R100 - R150
+      </label>
+
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test2" />
+        <span className="checkmark"></span>Over R150
+      </label>
+    </div>
+  );
 };
 
 export default Price;
